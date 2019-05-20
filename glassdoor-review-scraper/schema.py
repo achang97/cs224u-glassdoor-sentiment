ec5c6acb2611b95db0424e7ac93d3f81ae564cb1
Andrew Chang <achang97@stanford.edu>
2019-05-19 21:33:20 -0700

    Edited Glassdoor data scraper and got ~80K lines of initial data.

---
+++ glassdoor-review-scraper/schema.py
@@ -0,0 +1,14 @@
+SCHEMA = [
+    'date',
+    'employee_title',
+    'review_title',
+    'pros',
+    'cons',
+    'advice_to_mgmt',
+    'rating_overall',
+    'rating_balance',
+    'rating_culture',
+    'rating_career',
+    'rating_comp',
+    'rating_mgmt',
+]
